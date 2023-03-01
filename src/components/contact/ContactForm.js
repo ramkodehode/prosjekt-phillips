6ac3514cbdb4057353d6e506078b859d68053f00
ramkodehode <ramkodehode@gmail.com>
2023-03-01 15:02:52 +0100

    Implented styles and EmailJS

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,32 +1,71 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import styled from 'styled-components';
 
 const Container = styled.section`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  height: 800px;
-  border: 2px blue solid;
-`;
-
-const Form = styled.form`
-  border: 2px red solid;
-  padding: 20px;
-  margin: 10px;
-  display: flex;
-  align-items: center;
-
   @media only screen and (max-width: 768px) {
-    border-color: green;
-    width: 100%;
-    height: 600px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    height: 800px;
+    border: 2px blue solid;
   }
 `;
 
+const Form = styled.form`
+  @media only screen and (max-width: 768px) {
+    border-color: green;
+
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const Label = styled.label``;
+
+const InputName = styled.input``;
+const InputEmail = styled.input``;
+const TextArea = styled.textarea``;
+const SubmitButton = styled.input``;
+
 function ContactForm() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID',
+        'template_pg7job2',
+        form.current,
+        'user_eDz6z61ypctV8WwCbGfsH'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <Container>
-        <Form>ContactForm</Form>
+        <Form ref={form} onSubmit={sendEmail}>
+          <Label>Navn</Label>
+          <InputName type="text" name="user_name" required />
+          <Label>E-post</Label>
+          <InputEmail type="email" name="user_email" required />
+          <Label>Melding</Label>
+          <TextArea name="message" required />
+          <SubmitButton type="submit" value="Send" />
+        </Form>
       </Container>
     </>
   );
