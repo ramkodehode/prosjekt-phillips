7127f394058b56f39e569fd0e66d98dedd2bb3ea
ramkodehode <ramkodehode@gmail.com>
2023-03-16 14:45:22 +0100

    Added styled file

---
+++ src/components/contact/Contact.styled.js
@@ -0,0 +1,43 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+body {
+
+  background-color: #FCD19B;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  padding: 1em;
+}
+`;
+
+export const ContactInfo = styled.div``;
+export const Ul = styled.ul``;
+export const Li = styled.li``;
+export const Contact = styled.div``;
+
+export const Heading = styled.h1``;
+export const HeadingTwo = styled.h2``;
+
+export const Paragraph = styled.p``;
+
+export const Container = styled.div`
+  max-width: 1170px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1em;
+`;
+
+export const Wrapper = styled.div``;
+
+export const Form = styled.form``;
+
+export const Label = styled.label``;
+
+export const InputName = styled.input``;
+export const InputEmail = styled.input``;
+export const TextArea = styled.textarea``;
+export const SubmitButton = styled.input``;
+
+export const Message = styled.p``;
