cd2ea45cd79d58eafcd6738bb31ae7eaaeadf388
ramkodehode <ramkodehode@gmail.com>
2023-03-23 13:26:11 +0100

    Added styled

--- src/components/contact/Contact.styled.js
+++ src/components/contact/Contact.styled.js
@@ -6,21 +6,38 @@ export const GlobalStyle = createGlobalStyle`
 }
 body {
 
-  background-color: #FCD19B;
+  background-color: #ffcb8d;
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   padding: 1em;
 }
 `;
 
-export const ContactInfo = styled.div``;
-export const Ul = styled.ul``;
-export const Li = styled.li``;
-export const Contact = styled.div``;
+export const ContactInfo = styled.div`
+  background: #fcd19b;
+  text-align: center;
+  margin: 0 0 rem 0;
+`;
 
-export const Heading = styled.h1``;
+export const Contact = styled.div`
+  background: white;
+`;
+
+export const Heading = styled.h1`
+  text-align: center;
+`;
 export const HeadingTwo = styled.h2``;
 
-export const Paragraph = styled.p``;
+export const Span = styled.span`
+  color: #ffbb40;
+`;
+
+export const Paragraph = styled.p`
+  margin: 0;
+`;
+
+export const Full = styled.div`
+  grid-column: 1 / 3;
+`;
 
 export const Container = styled.div`
   max-width: 1170px;
@@ -29,15 +46,71 @@ export const Container = styled.div`
   padding: 1em;
 `;
 
-export const Wrapper = styled.div``;
+export const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+export const Li = styled.li``;
 
-export const Form = styled.form``;
+export const Wrapper = styled.div`
+  box-shadow: 0 0 20px 0 rgba(72, 94, 116, 0.7);
 
-export const Label = styled.label``;
+  > * {
+    padding: 1em;
+  }
 
-export const InputName = styled.input``;
-export const InputEmail = styled.input``;
-export const TextArea = styled.textarea``;
-export const SubmitButton = styled.input``;
+  @media (min-width: 700px) {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+
+    > * {
+      padding: 2em;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+`;
+
+export const Label = styled.label`
+  display: block;
+`;
+
+export const InputName = styled.input`
+  width: 100%;
+  padding: 1em;
+  border: 1px solid #fbd19c;
+`;
+export const InputEmail = styled.input`
+  width: 100%;
+  padding: 1em;
+  border: 1px solid #fbd19c;
+`;
+export const TextArea = styled.textarea`
+  width: 100%;
+  padding: 1em;
+  border: 1px solid #fbd19c;
+`;
+
+export const SubmitButton = styled.input`
+  background: #fbd19c;
+  border: 0;
+  text-transform: uppercase;
+
+  width: 100%;
+  padding: 1em;
+  border: 1px solid #fbd19c;
+
+  &:hover,
+  &:focus {
+    background: #ffa500;
+    color: #fff;
+    outline: 0;
+    transition: background-color 0.5s ease-out;
+  }
+`;
 
 export const Message = styled.p``;
