16539efb5c679b0f3a8b16a91a68207f2104d1f4
ramkodehode <ramkodehode@gmail.com>
2023-03-29 18:42:12 +0200

    Latest change

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,49 @@
-import { NavLink } from 'react-router-dom';
-import { Header, List, Nav, UnorderedList } from './Navbar.styled';
+import { Link, NavLink } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+import {
+  Anchor,
+  Header,
+  Item,
+  Label,
+  List,
+  LogoContainer,
+  LogoImage,
+  MobileIcon,
+  Nav,
+  UnorderedList,
+} from './Navbar.styled';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
       <Header>
         <Nav>
+          <LogoContainer>
+            <LogoImage></LogoImage>
+            <Label>Phillips Karriere</Label>
+          </LogoContainer>
+
+          <MobileIcon onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
           <UnorderedList>
-            <List>
-              <NavLink>Hjem</NavLink>
-            </List>
-            <List>
-              <NavLink>Tjenester</NavLink>
-            </List>
-            <List>
-              <NavLink>Om oss</NavLink>
-            </List>
-            <List>
-              <NavLink>Kontakt</NavLink>
-            </List>
+            <Item>
+              <Anchor>Hjem</Anchor>
+            </Item>
+            <Item>
+              <Anchor>Tjenester</Anchor>
+            </Item>
+            <Item>
+              <Anchor>Om oss</Anchor>
+            </Item>
+            <Item>
+              <Anchor>Kontakt</Anchor>
+            </Item>
           </UnorderedList>
         </Nav>
       </Header>
--- src/components/navbar/Navbar.styled.js
+++ src/components/navbar/Navbar.styled.js
@@ -1,12 +1,76 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  * {
+    padding: 0;
+    margin: 0;
+    text-decoration: 0;
+    list-style: none;
+    box-sizing: border-box;
+  }
+`;
 
-export const Nav = styled.nav``;
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: auto;
+  margin: 0;
+`;
 
-export const UnorderedList = styled.ul``;
+export const LogoImage = styled.img`
+  margin-left: 20px;
+`;
 
-export const List = styled.li``;
+export const Label = styled.label`
+  font-size: 20px;
+  line-height: 75px;
+  padding-left: 10px;
+  font-weight: bold;
+  color: black;
+`;
 
-export const NavLink = styled(Link)``;
+export const MobileIcon = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-100%, 60%);
+  font-size: 1.8rem;
+  cursor: pointer;
+`;
+
+export const Nav = styled.nav`
+  height: 75px;
+  position: fixed;
+
+  width: 100%;
+  background-color: #fad4a3;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const UnorderedList = styled.ul`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  top: 75px;
+  float: right;
+  background-color: #fad4a3;
+  text-align: center;
+`;
+
+export const Item = styled.li`
+  line-height: 30px;
+  opacity: 1;
+  margin: 50px 0;
+`;
+
+export const Anchor = styled.a`
+  color: white;
+  font-size: 20px;
+
+  &:hover {
+    color: black;
+  }
+`;
