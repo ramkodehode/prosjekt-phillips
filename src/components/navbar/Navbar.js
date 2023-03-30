5468f66d57f454ed08e491106c6c9270d56333df
ramkodehode <ramkodehode@gmail.com>
2023-03-30 14:46:02 +0200

    New style for navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,35 @@
-import { Link, LogoContainer, Nav, NavbarContainer } from './Navbar.styled';
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarInnerContainer,
+  NavbarExtendedContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  Logo,
+} from './Navbar.styled';
+
+import LogoImg from '../img/logo.png';
 
 const Navbar = () => {
   return (
     <>
       <NavbarContainer>
-        <LogoContainer>Phillips Karriere</LogoContainer>
-        <Nav>
-          <Link>Hjem</Link>
-          <Link>Tjenester</Link>
-          <Link>Om oss</Link>
-          <Link>Kontakt</Link>
-        </Nav>
+        <NavbarInnerContainer>
+          <LeftContainer>
+            <Logo src={LogoImg}></Logo>
+            <h3> Phillips Karriere</h3>
+          </LeftContainer>
+          <RightContainer>
+            <NavbarLinkContainer>
+              <NavbarLink>Hjem</NavbarLink>
+              <NavbarLink>Tjenester</NavbarLink>
+              <NavbarLink>Om oss</NavbarLink>
+              <NavbarLink>Kontakt</NavbarLink>
+            </NavbarLinkContainer>
+          </RightContainer>
+        </NavbarInnerContainer>
+        <NavbarExtendedContainer></NavbarExtendedContainer>
       </NavbarContainer>
     </>
   );
