0dc26eb937acca7120a4171e171f49906abb18d5
Sofie <sofie.kodehode@gmail.com>
2023-04-03 10:10:08 +0200

    forandret plassering på KomIgjen og deltaker

--- src/components/Hjem.js
+++ src/components/Hjem.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ContactForm from './contact/ContactForm';
-import Footer from './footer';
-import Intro from './Intro';
-import KomIgjen from './komIgjen';
-import KursDeltaker from './kursDeltaker';
-import Navbar from './navbar/Navbar';
-import PictureBoxes from './pictureBoxes';
-import GlobalStyle from './styles/Globalstyle';
-import Tjenester from './Tjenester';
-import InfoTrude from './trudeInfo';
-import VartAnsvar from './vartAnsvar';
-import Veiledning from './Veiledning';
+import React from "react";
+import ContactForm from "./contact/ContactForm";
+import Footer from "./footer";
+import Intro from "./Intro";
+import KomIgjen from "./komIgjen";
+import KursDeltaker from "./kursDeltaker";
+import Navbar from "./navbar/Navbar";
+import PictureBoxes from "./pictureBoxes";
+import GlobalStyle from "./styles/Globalstyle";
+import Tjenester from "./Tjenester";
+import InfoTrude from "./trudeInfo";
+import VartAnsvar from "./vartAnsvar";
+import Veiledning from "./Veiledning";
 
 const Hjem = () => {
   return (
@@ -22,9 +22,9 @@ const Hjem = () => {
       <Tjenester />
       <Veiledning />
       <InfoTrude />
+      <KursDeltaker /> {/* <------------ Her skal Karuselen inn Monica */}
       <KomIgjen />
       <PictureBoxes />
-      <KursDeltaker /> {/* <------------ Her skal Karuselen inn Monica */}
       <ContactForm />
       <Footer />
     </>
