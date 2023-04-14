f5d9b810d7f4d16a021066fbc0a464bc8ec05ef9
Sofie <sofie.kodehode@gmail.com>
2023-04-14 11:36:07 +0200

    stort undervisning bilde

--- src/components/Hjem.js
+++ src/components/Hjem.js
@@ -5,7 +5,7 @@ import Intro from "./Intro";
 import KomIgjen from "./komIgjen";
 import KursDeltaker from "./kursDeltaker";
 import Navbar from "./navbar/Navbar";
-import PictureBoxes from "./pictureBoxes";
+
 import GlobalStyle from "./styles/Globalstyle";
 import Tjenester from "./Tjenester";
 import InfoTrude from "./trudeInfo";
@@ -24,7 +24,6 @@ const Hjem = () => {
       <InfoTrude />
       <KursDeltaker /> {/* <------------ Her skal Karuselen inn Monica */}
       <KomIgjen />
-      <PictureBoxes />
       <ContactForm />
       <Footer />
     </>
--- src/components/komIgjen.js
+++ src/components/komIgjen.js
@@ -1,37 +1,66 @@
 import styled from "styled-components";
-import { Tittel } from "./styles/styledIntro";
 
-export const BoxToo = styled.div`
-  width: auto;
-  height: 25rem;
-  margin: 0 auto;
-  z-index: -1;
+import undervisningOP from "./img/undervisningOP.png";
 
-  background: #fbd19c;
+export const KomIgjenText = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  color: white;
 
   @media only screen and (max-width: 40em) {
-    width: auto;
-    height: 15rem;
+    font-size: 1rem;
+  }
+
+  @media only screen and (min-width: 2400px) {
+    font-size: 7rem;
   }
 `;
 
-export const KomIgjenText = styled.section`
-  padding: 5rem;
-  text-align: center;
+export const PictureBoxesContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+
+  padding: 50px;
+
+  position: relative;
+  top: -150px;
+
+  margin: 0 auto;
 
   @media only screen and (max-width: 40em) {
-    padding: 1rem;
+    display: none;
   }
+`;
+
+export const PicBoxes = styled.img`
+  width: 100%;
+  z-index: 0;
+
+  top: -100px;
+
+  @media only screen and (max-width: 40em) {
+  }
+`;
+
+export const DivImg = styled.div`
+  position: relative;
+  text-align: center;
+
+  padding-bottom: 100px;
 `;
 
 const KomIgjen = () => {
   return (
     <div>
-      <BoxToo>
-        <KomIgjenText>
-          <Tittel>Så la oss komme i gang!</Tittel>
-        </KomIgjenText>
-      </BoxToo>
+      <DivImg>
+        <KomIgjenText>Så la oss komme i gang!</KomIgjenText>
+        <PicBoxes src={undervisningOP} alt="clapsed arms" />
+      </DivImg>
     </div>
   );
 };
