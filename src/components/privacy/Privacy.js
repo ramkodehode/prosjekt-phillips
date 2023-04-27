948dc357ec51eae892c23c0a7db83f9ca7b6fb2a
monmath <mon_math@hotmail.com>
2023-04-27 13:42:18 +0200

    init

--- src/components/privacy/Privacy.js
+++ src/components/privacy/Privacy.js
@@ -16,7 +16,6 @@ import {
 } from './Privacy.styled';
 
 
-
 const Privacy = () => {
   return (
     <>
@@ -39,97 +38,76 @@ const Privacy = () => {
 </PolicyInfo>
     </PolicySection>
 
+    <PolicySection>
+      <PolicyLevel>
+        <p>Vi har tre ulike nivåer for personvern i våre nettløsninger. <br/> Den åpne nettsiden, 
+        når du ber om tilbud/forespør på studie- og eller karriereveileding og når vi inngår i et kundeforhold:</p>
+      </PolicyLevel>
+    </PolicySection>
 
 
-        <PolicySection>
-          <PolicyLevel>
-            <p>
-              Vi har tre ulike nivåer for personvern i våre nettløsninger.{' '}
-              <br /> Den åpne nettsiden, når du ber om tilbud/forespør på
-              studie- og eller karriereveileding og når vi inngår i et
-              kundeforhold:
-            </p>
-          </PolicyLevel>
-        </PolicySection>
 
-        <PolicySection>
-          <Ul>
-            <Li>
-              Denne nettsiden lagrer ingen personlig informasjon om deg som
-              bruker annet enn å måle og forstå nettrafikk for å gjøre sidene
-              bedre og mer tilgjengelig.
+    <PolicySection>
+      <Ul>
+        <Li>Denne nettsiden lagrer ingen personlig informasjon om deg som bruker annet enn å måle og 
+            forstå nettrafikk for å gjøre sidene bedre og mer tilgjengelig.
             </Li>
 
-            <Li>
-              Når du kontakter oss via kontaktsiden eller i skjemaet for
-              kontakt/forespørsel om studie- og eller karriereveileding lagres
-              denne informasjonen trygt og behandles fortrolig.
-              <br />
-              Vi er godt rutinerte med å behandle informasjon belagt med
-              taushetsplikt. Så fort vi har opprettet dialog blir denne
-              informasjonen rutinemessig slettet.
+        <Li>Når du kontakter oss via kontaktsiden eller i skjemaet for kontakt/forespørsel om studie- 
+            og eller karriereveileding lagres denne informasjonen trygt og behandles fortrolig. 
+            <br/>Vi er  godt rutinerte med å behandle informasjon belagt med taushetsplikt.    
+            Så fort vi har opprettet dialog blir denne informasjonen rutinemessig slettet.
             </Li>
 
-            <Li>
-              Når du har avtalt tid for veilednings inngår vi i et kundeforhold.
-              Da lagrer vi så lite vi kan, men akkurat nok til å oppfylle våre
-              forpliktelser overfor deg som kunde.
-              <br />
-              Det krever norske lover som i tur er innenfor GDPR.
+        <Li>Når du har avtalt tid for veilednings inngår vi i et kundeforhold. 
+            Da lagrer vi så lite vi kan, men akkurat nok til å oppfylle våre forpliktelser overfor deg som kunde. 
+            <br/>Det krever norske lover som i tur er innenfor GDPR.
             </Li>
-          </Ul>
-        </PolicySection>
+      </Ul>
+    </PolicySection>
 
-        <PolicySection>
-          <PolicySubTitle>
-            <h2>Bilder og video</h2>
-          </PolicySubTitle>
-          <p>De som brukes på nettsidene.</p>
-          <p>
-            1. På nettsiden brukes kun bilder der rettighetshaver (fotografen),
-            og den som er avbildet har godkjent bruken.
-          </p>
-        </PolicySection>
 
-        <PolicySection>
-          <PolicySubTitle>
-            <h2>Innebygd innhold fra andre nettsteder</h2>
-          </PolicySubTitle>
-          <p>
-            Artikler på denne siden kan inkludere innebygd innhold (f.eks.
-            videoer, bilder, artikler osv.). Innebygd innhold fra andre
-            nettsteder oppfører seg på nøyaktig samme måte som om den besøkende
-            hadde besøkt nettstedet som det innebygde innholdet kommer fra.
-            Disse nettstedene kan samle inn opplysninger om deg, bruke
-            informasjonskapsler, bygge inn sporingssystemer fra tredjeparter og
-            overvåke hva du gjør via dette innebygde innholdet. Dette omfatter
-            også sporing av handlingene dine via det innebygde innholdet dersom
-            du har en konto og er logget inn på nettstedet.
-          </p>
-        </PolicySection>
 
-        <PolicySection>
-          <PolicySubTitle>
-            <h2>Hvem vi deler dine opplysninger med</h2>
-          </PolicySubTitle>
-          <p>
-            Vi deler ingen opplysninger med andre annet enn nødvendig
-            faktureringsinformasjon i vårt regnskapssystem (Fiken.no).{' '}
-          </p>
-        </PolicySection>
+    <PolicySection>
+      <PolicySubTitle>
+        <h2>Bilder og video</h2>
+      </PolicySubTitle>
+        <p>De som brukes på nettsidene.</p>
+        <p>1.	På nettsiden brukes kun bilder der rettighetshaver (fotografen), 
+        og den som er avbildet har godkjent bruken.</p>
+    </PolicySection>
 
-        <PolicySection>
-          <PolicySubTitle>
-            <h2>Hvor lenge vi oppbevarer opplysninger om deg</h2>
-          </PolicySubTitle>
-          <p>
-            Vi sletter informasjon rutinemessig så snarlig som mulig. For
-            forespørsler til oss som inneholder personlig informasjon vil dette
-            slettes så fort videre dialog er igangsatt. Når vi inngår i et
-            kundeforhold må vi forholde oss til regnskapsloven og
-            personvernforordningene i Norge. Disse er i samsvar med GDPR.
-          </p>
-        </PolicySection>
+
+    <PolicySection>
+      <PolicySubTitle>
+        <h2>Innebygd innhold fra andre nettsteder</h2>
+      </PolicySubTitle>
+        <p>Artikler på denne siden kan inkludere innebygd innhold (f.eks. videoer, bilder, artikler osv.). 
+          Innebygd innhold fra andre nettsteder oppfører seg på nøyaktig samme måte som om den besøkende hadde 
+          besøkt nettstedet som det innebygde innholdet kommer fra.
+          Disse nettstedene kan samle inn opplysninger om deg, bruke informasjonskapsler, bygge inn sporingssystemer 
+          fra tredjeparter og overvåke hva du gjør via dette innebygde innholdet. Dette omfatter også sporing av handlingene 
+          dine via det innebygde innholdet dersom du har en konto og er logget inn på nettstedet.</p>
+    </PolicySection>
+
+
+    <PolicySection>
+      <PolicySubTitle>
+        <h2>Hvem vi deler dine opplysninger med</h2>
+      </PolicySubTitle>
+        <p>Vi deler ingen opplysninger med andre annet enn nødvendig faktureringsinformasjon i vårt regnskapssystem (Fiken.no). </p>
+    </PolicySection>
+
+
+    <PolicySection>
+      <PolicySubTitle>
+        <h2>Hvor lenge vi oppbevarer opplysninger om deg</h2>
+      </PolicySubTitle>
+        <p>Vi sletter informasjon rutinemessig så snarlig som mulig. For forespørsler til oss som inneholder personlig informasjon 
+        vil dette slettes så fort videre dialog er igangsatt. Når vi inngår i et kundeforhold må vi forholde oss til 
+        regnskapsloven og personvernforordningene i Norge. 
+        Disse er i samsvar med GDPR.</p>
+      </PolicySection>
 
   <PolicySection>
     <PolicySubTitle>
@@ -144,10 +122,14 @@ const Privacy = () => {
 
 </PolicyContainer>
 
-  <PrivacyNavbar />
+<Footer/>
   </>
 
   );
+
 };
 
 export default Privacy;
+
+
+
