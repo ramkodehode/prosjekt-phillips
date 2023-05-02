114cc8cbfc649328cdc55b7d35a02160a30825cf
monmath <mon_math@hotmail.com>
2023-05-02 11:58:07 +0200

    Tatt bort en kursdeltaker

--- src/components/kursDeltaker.js
+++ src/components/kursDeltaker.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import IMG01 from './img/kursdeltaker01.jpg';
-import IMG02 from './img/kursdeltaker02.jpg';
-import IMG03 from './img/kursdeltaker03.jpg';
-import IMG04 from './img/kursdeltaker04.jpg';
-import IMG05 from './img/kursdeltaker05.jpg';
-import IMG06 from './img/kursdeltaker06.jpg';
-import IMG07 from './img/kursdeltaker07.jpg';
+import IMG02 from './img/kursdeltaker03.jpg';
+import IMG03 from './img/kursdeltaker04.jpg';
+import IMG04 from './img/kursdeltaker05.jpg';
+import IMG05 from './img/kursdeltaker06.jpg';
+import IMG06 from './img/kursdeltaker07.jpg';
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -29,39 +28,33 @@ const data = [
   },
   {
     image: IMG02,
-    name: "Deborah",
-    review:
-      "Trude sin ekspertise, kunnskap, støtte, og ikke minst klokskap kunne jeg ikke vært foruten da jeg ble rammet av nedbemanning. I denne perioden bidro hun til å gi meg troen tilbake på egne styrker og kvaliteter. Jobber i dag som Key Account Manager.",
-  },
-  {
-    image: IMG03,
     name: "Mathilde",
     review:
       "Veiledningen hjalp meg  å finne retning når jeg stod i noe som føltes ut som et umulig veivalg. Nå er jeg i gang med siste året på politiutdanningen som jeg opplever er et veldig riktig valg for meg!",
   },
   {
-    image: IMG04,
+    image: IMG03,
     name: "William",
     review:
       "Hadde det ikke vært for Trude  hadde jeg ikke funnet min vei gjennom studier og arbeidsplassen jeg trives så godt på i dag! Jobber i dag som eiendomsmegler og studere ledelse og samfunnsøkonomi.",
   },
   {
-    image: IMG05,
+    image: IMG04,
     name: "Oda",
     review:
       "Eg var usikker på kva eg ville etter vidaregåande og fekk kartlegging og veiledning av Trude. Det endte med at eg søkte ei utdanning eg aldri hadde hørt om. Det var full klaff! Etter fire år i arbeid og to vidareutdanningar innan feltet ergoterapi stortrivast eg med valet eg tok.",
   },
   {
-    image: IMG06,
+    image: IMG05,
     name: "Sondre ",
     review:
       "Veiledningen hjalp meg å få oversikt, og jeg vil særlig trekke frem de gode samtalene vi hadde og personlighetstestene vi brukte for å kartlegge egenskapene og interessene mine. I tillegg fikk jeg veldig god hjelp til søknadsskriving og cv. Jobber i dag som offiser i Kystvakten.",
   },
   {
-    image: IMG07,
+    image: IMG06,
     name: "Thomas",
     review:
-      "Å kontakte Trude åpnet dører jeg ikke visste fantes, som har ført til to spennende jobber og videre karrieremuligheter i det selskapet jeg jobber i dag. Uten Trude hadde jeg aldri vært der jeg er i dag- og mulighetene mine fremover er mange. Tusen takk!",
+      " Å kontakte Trude åpnet dører jeg ikke visste fantes, som har ført til to spennende jobber og videre karrieremuligheter i det selskapet jeg jobber i dag. Uten Trude hadde jeg aldri vært der jeg er i dag- og mulighetene mine fremover er mange. Tusen takk!  ",
   },
 ];
 
