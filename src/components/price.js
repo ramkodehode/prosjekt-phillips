d830f905b0bc49a3b3a9b30b3f989ed3e1054f09
ramkodehode <ramkodehode@gmail.com>
2023-05-10 13:24:53 +0200

    Layout change arbeidsgiver

--- src/components/price.js
+++ src/components/price.js
@@ -13,6 +13,7 @@ import PriceBoxTwo from './pricePage/ProceBoxTwo';
 import { ContainerPhone } from './styles/styledTjenester';
 
 import styled from 'styled-components';
+import Arbeidsgiver from './arbeidsgiverFase/Arbeidsgiver';
 
 export const UnderTitleArbeidsgiver = styled.h2`
   font-style: normal;
@@ -63,7 +64,8 @@ const PriceBox = () => {
           </NewSection>
         </Container>
       </DivPrice>
-      <DivPhoneArbeidsgiver>
+      <Arbeidsgiver />
+      {/* <DivPhoneArbeidsgiver>
         <TextTaKontakt>For arbeidsgiver</TextTaKontakt>
         <TextArbeidsgiver>
           Etter inngått avtale starter vi med oppstartsmøte hvor vi foretar en
@@ -79,7 +81,7 @@ const PriceBox = () => {
           <UnderTitleArbeidsgiver>Fase 3</UnderTitleArbeidsgiver>
           Evaluering.
         </TextArbeidsgiver>
-      </DivPhoneArbeidsgiver>
+      </DivPhoneArbeidsgiver> */}
     </div>
   );
 };
