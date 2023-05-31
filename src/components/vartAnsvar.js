6439ddfea6a33e7891c224ffbcd0061ce6e353fd
Sofie <sofie.kodehode@gmail.com>
2023-05-31 12:54:54 +0200

    Texted changed vårtAnsvar from Trude

--- src/components/vartAnsvar.js
+++ src/components/vartAnsvar.js
@@ -1,6 +1,6 @@
-import { IntroText } from "./styles/styledIntro";
+import { IntroText, UnderTitle } from "./styles/styledIntro";
 import { TitleTrude } from "./styles/styledTrude";
-import { VartAnsvarContainer } from "./styles/styledVartAnsvar";
+import { Rectangle, VartAnsvarContainer } from "./styles/styledVartAnsvar";
 
 const VartAnsvar = () => {
   return (
@@ -10,27 +10,29 @@ const VartAnsvar = () => {
           Vi gir deg verktøyene du trenger for veien videre.
         </TitleTrude>
 
+        <UnderTitle>
+          <Rectangle />
+          Stadige endringer i arbeidslivet byr på utfordringer og nye
+          muligheter.{" "}
+        </UnderTitle>
+
         <IntroText>
-          De unge kjenner på usikkerhet i valg av høyere utdanning.
-          Arbeidsmarkedet er i rask endring og gjør at flere opplever endret
-          arbeidssituasjon. Mange arbeidsgivere ser verdien av å tilby ansatte
-          karriereveiledning i forbindelse med omstilling. Det skaper verdi for
-          den enkelte medarbeider, bedriftens omdømme og for samfunnet.
+          Mange unge kjenner på usikkerhet i valg av høyere utdanning, og
+          arbeidstakere opplever hyppigere omstillig. I dette landskapet skal
+          den enkelte ta valg og håndtere utfordringer knyttet til utdanning og
+          arbeid. Det krever å finne ut av hva en ønsker, har interesse for,
+          egne verdier, hva en kan og hva en er god til. Karrierekompetanse
+          bidrar til nettopp dette. Det er blant annet kompetanse til å kjenne
+          og forstå selv selv og sin kontekst, ta valg og ta ansvar for sine
+          valg.
         </IntroText>
         <IntroText>
-          I vår veiledning møter vi deg der du er, og vektlegger en trygg ramme
-          basert på gjensidig tillit. Din læring frem mot egne mål er det
-          sentrale om det gjelder studievalg, ny jobb, eller en kombinasjon. I
-          samspillet med karriereveileder er du den aktive i prosessen. Våre
-          karrierelæringsprogram er tilrettelagt individuelt, for grupper og i
-          kombinasjonen av fysisk og digitale møter. Undervisning, verktøy og
-          metodikk følger Nasjonalt kvalitetsrammeverk for karriereveiledning og
-          DNV sertifisering.
-        </IntroText>
-        <IntroText>
-          {" "}
-          Representerer du arbeidsgiver møter vi deg for behovsavklaring og
-          følger raskt opp med tilbud basert på dine behov.
+          Vårt utgangspunkt er å møte deg der du er med respekt, interesse og
+          nysgjerrighet. Det viktige er din læring frem mot nye muligheter om
+          det gjelder studievalg, ny jobb, eller en kombinasjon. Du er den
+          aktive i samspillet med karriereveileder. I tråd med Nasjonalt
+          kvalitetsrammeverk for karriereveiledning skal vi bidra til
+          karrierekompetanse gjennom utforskning og læring.
         </IntroText>
       </VartAnsvarContainer>
     </div>
