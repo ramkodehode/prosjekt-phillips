7ac915cbe1cff4accdb61c65880c2c75a9d33def
Sofie <sofie.kodehode@gmail.com>
2023-07-03 12:43:09 +0200

    added link to facebook in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,6 +8,32 @@ import {
   ImgKvalitet,
   StyledLink,
 } from "./styles/styledFooter";
+import { AiFillFacebook } from "react-icons/ai";
+
+import styled from "styled-components";
+
+export const DivFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LinkFacebook = styled.a`
+  text-decoration: none;
+
+  color: white;
+
+  font-size: 2rem;
+
+  &:hover {
+    color: #e5701b;
+  }
+`;
+
+export const DivFacebook = styled.div`
+  padding: 1rem;
+`;
+
+export const FooterLabel = styled.label``;
 
 const Footer = () => {
   return (
@@ -19,8 +45,19 @@ const Footer = () => {
             <OrgNr>829590352. </OrgNr>
           </p>
 
-          <StyledLink to="/privacy">Personvernerklæring</StyledLink>
+          <DivFooter>
+            <StyledLink to="/privacy">Personvernerklæring</StyledLink>
 
+            <DivFacebook>
+              <LinkFacebook
+                href="https://www.facebook.com/profile.php?id=100094047773942"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiFillFacebook />
+              </LinkFacebook>
+            </DivFacebook>
+          </DivFooter>
           <p>
             Copyright 2023 © All Rights Reserved. <br />
             Built by K3
