1804f060910bf723ce949cd3c8978b6a905ab3a8
Sofie <sofie.kodehode@gmail.com>
2023-07-03 13:35:43 +0200

    added link to instagram to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ import {
   ImgKvalitet,
   StyledLink,
 } from "./styles/styledFooter";
-import { AiFillFacebook } from "react-icons/ai";
+import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
 
 import styled from "styled-components";
 
@@ -17,23 +17,23 @@ export const DivFooter = styled.div`
   flex-direction: column;
 `;
 
-export const LinkFacebook = styled.a`
+export const LinkMedia = styled.a`
   text-decoration: none;
 
   color: white;
 
   font-size: 2rem;
 
+  margin: 0.6rem;
+
   &:hover {
     color: #e5701b;
   }
 `;
 
-export const DivFacebook = styled.div`
+export const DivMedia = styled.div`
   padding: 1rem;
 `;
-
-export const FooterLabel = styled.label``;
 
 const Footer = () => {
   return (
@@ -48,15 +48,23 @@ const Footer = () => {
           <DivFooter>
             <StyledLink to="/privacy">Personvernerklæring</StyledLink>
 
-            <DivFacebook>
-              <LinkFacebook
+            <DivMedia>
+              <LinkMedia
                 href="https://www.facebook.com/profile.php?id=100094047773942"
                 target="_blank"
                 rel="noreferrer"
               >
                 <AiFillFacebook />
-              </LinkFacebook>
-            </DivFacebook>
+              </LinkMedia>
+
+              <LinkMedia
+                href="https://instagram.com/phillips_karriere?igshid=NTc4MTIwNjQ2YQ=="
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiFillInstagram />
+              </LinkMedia>
+            </DivMedia>
           </DivFooter>
           <p>
             Copyright 2023 © All Rights Reserved. <br />
